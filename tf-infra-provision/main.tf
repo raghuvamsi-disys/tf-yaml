2c5e018d1800166b8e3595025edfdad783934b58
raghuvamsi-disys <64777929+raghuvamsi-disys@users.noreply.github.com>
2021-10-26 18:17:15 +0530

    Update main.tf

--- tf-infra-provision/main.tf
+++ tf-infra-provision/main.tf
@@ -1,3 +1,10 @@
 locals {
     
 }
+
+resource "azurerm_data_factory" "example" {
+   name                = 'yamldf'
+   location            = 'westus'
+   resource_group_name = 'dev-provisioning-rg'
+   
+}
